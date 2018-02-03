9255599ff68aa9003c90910910f8db6c0fc92de8
sammiej <jiangsammie@gmail.com>
2018-02-02 23:43:33 -0800

    Fixing up extra files

--- CPEN-311/Lab 4 Decryption/Lab 4 - Original/simulation/modelsim/rtl_work/e_memory/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity e_memory is
-    port(
-        address         : in     vl_logic_vector(4 downto 0);
-        clock           : in     vl_logic;
-        q               : out    vl_logic_vector(7 downto 0)
-    );
-end e_memory;
--- CPEN-311/Lab 4 Decryption/Lab 4 - Original/simulation/qsim/work/ksa_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity ksa_vlg_vec_tst is
-end ksa_vlg_vec_tst;
